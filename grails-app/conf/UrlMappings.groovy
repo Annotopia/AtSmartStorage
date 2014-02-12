0bdcddb496b6dd47fbbe47e137f71c0fac9ba8a6
Paolo Ciccarese <paolo.ciccarese@gmail.com>
2014-02-12 14:09:13 -0500

    Mapping to Annotation Set and Annotation RESTful API

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -1,6 +1,15 @@
 class UrlMappings {
 
 	static mappings = {
+		"/annotationset/$id?"{
+			controller = "annotationSet"
+			action = [GET:"show", POST:"save"]
+		}
+		"/annotation/$id?"{
+			controller = "annotation"
+			action = [GET:"show", POST:"save"]
+		}
+		
 		"/$controller/$action?/$id?"{
 			constraints {
 				// apply constraints here
