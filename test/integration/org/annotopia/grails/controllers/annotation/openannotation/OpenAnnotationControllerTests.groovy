f5245092eea3f008353a62c4e3837a8d2b914df5
Paolo Ciccarese <paolo.ciccarese@gmail.com>
2014-02-24 16:04:14 -0500

    Moved OpenAnnotationController

--- test/integration/org/annotopia/grails/controllers/annotation/openannotation/OpenAnnotationControllerTests.groovy
+++ test/integration/org/annotopia/grails/controllers/annotation/openannotation/OpenAnnotationControllerTests.groovy
@@ -18,7 +18,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.annotopia.grails.controllers.annotation;
+package org.annotopia.grails.controllers.annotation.openannotation;
 
 import static org.junit.Assert.*
 import grails.test.mixin.TestFor
