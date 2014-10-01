84c454a86accd18949569e6355341b4246ccf89a
Paolo Ciccarese <paolo.ciccarese@gmail.com>
2014-10-01 11:09:15 -0400

    Updated the dependencies. Jena 2.11.2 has been released

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -13,17 +13,7 @@ grails.project.dependency.resolution = {
     legacyResolve false // whether to do a secondary resolve on plugin installation, not advised and here for backwards compatibility
     repositories {
         grailsCentral()
-        // uncomment the below to enable remote dependency resolution
-        // from public Maven repositories
-        //mavenLocal()
         mavenCentral()
-        //mavenRepo "http://snapshots.repository.codehaus.org"
-        //mavenRepo "http://repository.codehaus.org"
-        //mavenRepo "http://download.java.net/maven/2/"
-        //mavenRepo "http://repository.jboss.com/maven2/"
-		
-		//mavenRepo "http://build.mygrid.org.uk/maven/repository/"
-		mavenRepo "https://repository.apache.org/content/repositories/snapshots/"
     }
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
@@ -31,20 +21,14 @@ grails.project.dependency.resolution = {
 		runtime 'virtuoso:virtjdbc:4'
 		runtime 'virtuoso.sesame:virt_jena:2'
 
-		compile ("org.apache.jena:jena-core:2.11.2-SNAPSHOT") {
+		compile ("org.apache.jena:jena-core:2.12.0") {
 			excludes 'slf4j-api', 'xercesImpl'
 		}
-		compile ("org.apache.jena:jena-arq:2.11.2-SNAPSHOT")
-		ile ("xml-apis:xml-apis:1.4.01") {
-			excludes 'xercesImpl'
-		}
-		test "org.spockframework:spock-grails-support:0.7-groovy-2.0"
+		compile ("org.apache.jena:jena-arq:2.12.0")
 		
 		compile("xerces:xercesImpl:2.9.1") {
 			excludes 'xml-apis'
-			}
-		 
-		 
+		}	 
     }
 
     plugins {
